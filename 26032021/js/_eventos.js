34fa6abfd3024a818d03097e5870be1f1bddb8cc
Katharina Pedrosa <katharinaandressa@hotmail.com>
2021-03-26 21:39:47 -0300

    segundo commit

---
+++ 26032021/js/_eventos.js
@@ -0,0 +1,18 @@
+$(function () {
+    $('.coordenadas').mouseenter(function () {
+        $('.valores').fadeIn();
+    });
+    $('.coordenadas').mouseenter(function (e) {
+        var posicoes = $(this).offset();
+        var x = e.clientX - posicoes.left;
+        var y = e.clientY - posicoes.top;
+        $('.valores').html(`coordenadas x: ${x} e y:${y}`);
+
+    });
+
+    $('.coordenadas').mouseout(function () {
+        $('.valores').fadeOut();
+    });
+
+
+});
